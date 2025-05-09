f027c7cb0f9c7c761fd9f5a7bd5bc6b766c9d79c
Jagadeesh147 <jagaadesh147@gmail.com>
2025-05-09 09:37:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+//  script.js
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("hidden");
+});
