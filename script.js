b98959feb1d5bd92035afd62dd10af3d08ef38af
Jagadeesh147 <jagaadesh147@gmail.com>
2026-07-19 13:44:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //  script.js
 document.getElementById("menu-toggle").addEventListener("click", function() {
     const menu = document.getElementById("menu");
-    menu.classList.toggle("hidden");
+    menu.classList.toggle("hidden");  
 });      
         
